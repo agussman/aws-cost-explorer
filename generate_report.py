f571aa40a0d5f5ffcdba0dd67d1ff749a2aa9438
Aaron Gussman <agussman@thehumangeo.com>
2018-01-29 23:58:59 -0500

    Basic query by 'Project' Tag

--- generate_report.py
+++ generate_report.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
 import boto3
+import datetime
+import pprint
 
-# Let's use Amazon S3
-s3 = boto3.resource('s3')
+client = boto3.client('ce')
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
+now = datetime.datetime.utcnow()
+start = (now - datetime.timedelta(days=2)).strftime('%Y-%m-%d')
+end = now.strftime('%Y-%m-%d')
+
+
+response = client.get_cost_and_usage(
+    TimePeriod={
+        'Start': start,
+        'End':  end
+    },
+    Granularity='DAILY',
+    Metrics=['BlendedCost'],
+    GroupBy=[
+        {
+            'Type': 'TAG',
+            'Key': 'Project'
+        },
+    ]
+)
+
+pprint.pprint(response)
