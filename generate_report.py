3db21b4d5769cba8a48627de712a873445b32943
Aaron Gussman <agussman@thehumangeo.com>
2018-01-30 23:11:31 -0500

    Timerange of previous month. Print project name and amount as currency

--- generate_report.py
+++ generate_report.py
@@ -3,12 +3,21 @@
 import boto3
 import datetime
 import pprint
+import re
 
 client = boto3.client('ce')
 
+# Set time range to cover the last full calendar month
+# Note that the end date is EXCLUSIVE (e.g., not counted)
 now = datetime.datetime.utcnow()
-start = (now - datetime.timedelta(days=2)).strftime('%Y-%m-%d')
-end = now.strftime('%Y-%m-%d')
+# Set the end of the range to start of the current month
+end = datetime.datetime(year=now.year, month=now.month, day=1)
+# Subtract a day and then "truncate" to the start of previous month
+start = end - datetime.timedelta(days=1)
+start = datetime.datetime(year=start.year, month=start.month, day=1)
+# Convert them to strings
+start = start.strftime('%Y-%m-%d')
+end = end.strftime('%Y-%m-%d')
 
 
 response = client.get_cost_and_usage(
@@ -16,7 +25,7 @@ response = client.get_cost_and_usage(
         'Start': start,
         'End':  end
     },
-    Granularity='DAILY',
+    Granularity='MONTHLY',
     Metrics=['BlendedCost'],
     GroupBy=[
         {
@@ -26,4 +35,14 @@ response = client.get_cost_and_usage(
     ]
 )
 
-pprint.pprint(response)
+#pprint.pprint()
+
+for project in response["ResultsByTime"][0]["Groups"]:
+    namestring = project['Keys'][0]
+    name = re.search("\$(.*)", namestring).group(1)
+    if name is None or name == "":
+        name = "Other"
+
+    amount = project['Metrics']['BlendedCost']['Amount']
+    amount = float(amount)
+    print("{}\t${:,.2f}".format(name, amount))
